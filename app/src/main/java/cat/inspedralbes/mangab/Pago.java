a6979cfb143c11b48b093b5af19522da1d9e3264
Gabriel <a18gabmantor@inspedralbes.cat>
2023-04-20 17:08:07 +0200

    Canvis

--- app/src/main/java/cat/inspedralbes/mangab/Pago.java
+++ app/src/main/java/cat/inspedralbes/mangab/Pago.java
@@ -2,13 +2,24 @@ package cat.inspedralbes.mangab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Pago extends AppCompatActivity {
 
+    EditText credit = findViewById(R.id.editTextNumberPassword);
+    EditText nif = findViewById(R.id.editTextTextPersonName7);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pago);
+    }
+
+    public void onClickCancelar(View view){
+
+        Intent intent = new Intent(this, Plato.class);
+        startActivity(intent);
     }
 }
